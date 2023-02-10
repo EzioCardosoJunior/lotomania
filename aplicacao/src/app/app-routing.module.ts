f0cd701e618888c0669a62e2bd4a8d3e5c7d4335
EzioCardosoJunior <ejcj1980@msn.com>
2023-02-10 20:45:03 -0300

    ContentComponent

--- aplicacao/src/app/app-routing.module.ts
+++ aplicacao/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './template/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"app-content",
+    component: ContentComponent
+  },
+  
+  {
+    path: '', 
+    redirectTo: 'app-content', 
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- aplicacao/src/app/app.module.ts
+++ aplicacao/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { ContentComponent } from './template/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
